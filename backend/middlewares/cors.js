5216c3d193bd102d966155753b43d01db99724cd
Nadezhda Platonova <platonova.nadezhda.webdev@yandex.ru>
2021-12-13 13:32:31 +0300

    fix cors

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,26 +1,26 @@
-const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+// const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
-const allowedCors = [
-  'https://platonova.mesto.nomoredomains.rocks',
-  'http://platonova.mesto.nomoredomains.rocks',
-  'localhost:3000',
-  'http://localhost:3000',
-];
+// const allowedCors = [
+//   'https://platonova.mesto.nomoredomains.rocks',
+//   'http://platonova.mesto.nomoredomains.rocks',
+//   'localhost:3000',
+//   'http://localhost:3000',
+// ];
 
-module.exports = (req, res, next) => {
-  const { method } = req;
-  const requestHeaders = req.headers['access-control-request-headers'];
-  const { origin } = req.headers;
+// module.exports = (req, res, next) => {
+//   const { method } = req;
+//   const requestHeaders = req.headers['access-control-request-headers'];
+//   const { origin } = req.headers;
 
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Credentials', 'true');
-  }
+//   if (allowedCors.includes(origin)) {
+//     res.header('Access-Control-Allow-Origin', origin);
+//     res.header('Access-Control-Allow-Credentials', 'true');
+//   }
 
-  if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-    res.end();
-  }
-  next();
-};
+//   if (method === 'OPTIONS') {
+//     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+//     res.header('Access-Control-Allow-Headers', requestHeaders);
+//     res.end();
+//   }
+//   next();
+// };
