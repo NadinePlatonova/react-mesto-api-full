34bd280b085b566d3e534be3de68ce8fc279a7e6
Nadezhda Platonova <platonova.nadezhda.webdev@yandex.ru>
2021-12-15 01:01:57 +0300

    fix: change README and add crash-test

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -12,8 +12,6 @@ const auth = (req, res, next) => {
   let payload;
 
   try {
-    console.log(NODE_ENV);
-    console.log(JWT_SECRET);
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : JWT_SECRET_DEV);
   } catch (err) {
     throw new UnauthorisedUserError('Необходима авторизация');
